2aa83721a7633e50fde08a48036fe76709a50ee6
Gabriel Vieira <gabrielcnt96@gmail.com>
2025-09-28 18:26:40 -0300

    feat: configurações de variaveis de ambiente criadas

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///banco.db")
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
