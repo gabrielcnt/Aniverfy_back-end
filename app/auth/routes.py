4156639e4b217920d5b44696c1a7285ca8d3189f
Gabriel Vieira <gabrielcnt96@gmail.com>
2025-10-15 10:31:58 -0300

    adicionando logs na rota de criar conta e gerando arquivo de logs

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, request, jsonify, current_app
+from flask import Blueprint, request, jsonify
+from logger import criar_logger
 from app import db
 from datetime import datetime
 from app.models.users import Usuario
@@ -6,6 +7,8 @@ from werkzeug.security import generate_password_hash
 from app.utils.validator import validar_senha, validar_email, validar_nome, validar_data_nascimento
 
 auth_bp = Blueprint('auth', __name__, url_prefix='/auth')
+
+register_log = criar_logger('Register')
 
 @auth_bp.route('/criar_conta', methods=['POST'])
 def registrar():
@@ -16,7 +19,6 @@ def registrar():
     - nome: Nome completo do usuário
     - email: Email válido e único
     - senha: Senha que atende aos requisitos mínimos
-    - repetir_senha: Confirmação da senha
     - data_nascimento: Data no formato DD-MM-YYYY
 
     Retorna:
@@ -26,14 +28,14 @@ def registrar():
     """
 
     try:
-        
-        current_app.logger.info(f"Dados recebidos: {request.form}")
-
+        register_log.info('Iniciando processo de registro')
+        register_log.debug("Método da requisição: {request.method}")
         nome = request.form.get("nome")
         email = request.form.get("email")
         data_de_nascimento = request.form.get("data_nascimento")
         senha = request.form.get("senha")
         
+        register_log.info(f"Dados recebidos: {request.form}")
 
         erros = []
 
@@ -52,9 +54,11 @@ def registrar():
         email_existente = Usuario.query.filter_by(email=email).first()
         
         if email_existente:
+            register_log.debug('Tentativa de cadastrar um email que ja esta no banco de dados')
             erros.append('E-mail já cadastrado')
 
         if erros:
+            register_log.error("erros de validação encontrados: {erros}")
             return jsonify({'erros': erros}), 400
         
         password_hash = generate_password_hash(senha)
@@ -71,12 +75,12 @@ def registrar():
             db.session.commit()
         except Exception as e:
             db.session.rollback()
-            current_app.logger.error(f'Erro ao salvar usuário. Detalhes: {str(e)}')
-            current_app.logger.error(f'Dados recebidos: {nome}, {email}, {data_de_nascimento}')
+            register_log.error(f'Erro ao salvar usuário. Detalhes: {str(e)}')
+            register_log.error(f'Dados recebidos: {nome}, {email}, {data_de_nascimento}')
             return jsonify({'erros': ['Erro ao salvar usuário']}), 500
 
         return jsonify({'mensagem': 'Usuario registrado com sucesso'}), 201
     
     except Exception as e:
-        current_app.logger.error(f'Erro interno no servidor: {str(e)}')
-        return jsonify({'erros': ['Erro interno no servidor']}), 500    
+        register_log.error(f'Erro interno no servidor: {str(e)}')
+        return jsonify({'erros': ['Erro interno no servidor']}), 500
